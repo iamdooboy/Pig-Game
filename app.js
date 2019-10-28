684afb38d6a758682cfaa9172e952284bb984469
Duy Le <asianinvasion.le@gmail.com>
2019-10-27 20:48:09 -0400

    added more logic and indicators when switching player

--- app.js
+++ app.js
@@ -39,6 +39,29 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 	
 	
 	//3. Update the round score if the rolled number was not a 1
+	if(dice !== 1){
+		//add score
+		roundScore += dice; 
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+	}else {
+		//next player
+		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+		roundScore = 0;
+		
+		document.getElementById('current-0').textContent = '0';
+		document.getElementById('current-1').textContent = '0';
+		
+		document.querySelector('.player-0-panel').classList.toggle('active');
+		document.querySelector('.player-1-panel').classList.toggle('active');
+		
+		//document.querySelector('.player-0-panel').classList.remove('active');
+		//document.querySelector('.player-1-panel').classList.add('active');
+		
+		document.QuerySelector('.dice').style.display = 'none';
+	}
+	
+	
+	
 });
 
 
