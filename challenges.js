1b8d410165f0a7e1e0da5d5bf0fae68adcc556b9
Duy Le <asianinvasion.le@gmail.com>
2019-10-28 14:43:13 -0400

    implemented challenge 3

--- challenges.js
+++ challenges.js
@@ -24,29 +24,40 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 	if (gamePlaying){
 		
 		//1. Random number
-		var dice = Math.floor(Math.random() * 6) + 1;
+		var dice1 = Math.floor(Math.random() * 6) + 1;
+		var dice2 = Math.floor(Math.random() * 6) + 1;
 		
 		//2. Display the result
-		var diceDOM = document.querySelector('.dice');
-		diceDOM.style.display = 'block';
-		diceDOM.src = 'dice-' + dice + '.png';
-
+		document.getElementById('dice-1').style.display = 'block';
+		document.getElementById('dice-2').style.display = 'block';
+		document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+		document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
 		
 		//3. Update the round score if the rolled number was not a 1
-		if (dice === 6 && previous === 6){
-			scores[activePlayer] = 0;
-			document.querySelector('#score-' + activePlayer).textContent = '0';
-			nextPlayer();
-		} else if(dice !== 1){
+		if(dice1 !== 1 && dice2 !== 1){
 			//add score
-			roundScore += dice; 
+			roundScore += dice1 + dice2; 
 			document.querySelector('#current-' + activePlayer).textContent = roundScore;
 		}else {
 			//next player
 			nextPlayer();
 		}
 		
-		previous = dice;
+//		//3. Update the round score if the rolled number was not a 1
+//		if (dice === 6 && previous === 6){
+//			scores[activePlayer] = 0;
+//			document.querySelector('#score-' + activePlayer).textContent = '0';
+//			nextPlayer();
+//		} else if(dice !== 1){
+//			//add score
+//			roundScore += dice; 
+//			document.querySelector('#current-' + activePlayer).textContent = roundScore;
+//		}else {
+//			//next player
+//			nextPlayer();
+//		}
+//		
+//		previous = dice;
 	}
 });
 
@@ -72,7 +83,8 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 		//check if player won the game 
 		if (scores[activePlayer] >= winningScore){
 			document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-			document.querySelector('.dice').style.display = 'none';
+			document.getElementById('dice-1').style.display = 'none';
+			document.getElementById('dice-2').style.display = 'none';
 			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
 			document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
 			gamePlaying = false;
@@ -98,7 +110,8 @@ function nextPlayer(){
 	//document.querySelector('.player-0-panel').classList.remove('active');
 	//document.querySelector('.player-1-panel').classList.add('active');
 		
-	document.querySelector('.dice').style.display = 'none';
+	document.getElementById('dice-1').style.display = 'none';
+	document.getElementById('dice-2').style.display = 'none';
 }
 
 document.querySelector('.btn-new').addEventListener('click', init);
@@ -114,7 +127,8 @@ function init() {
 	//document.querySelector('#current-' + activePlayer).innerHTML = 'em' + dice + '</em>';
 
 	//css
-	document.querySelector('.dice').style.display = 'none';
+	document.getElementById('dice-1').style.display = 'block';
+	document.getElementById('dice-2').style.display = 'block';
 
 	document.getElementById('score-0').textContent = '0';
 	document.getElementById('score-1').textContent = '0';
